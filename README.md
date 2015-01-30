4ce7cc1d1c7de881bd597204dd1090c4388d05fe
Jesse Baird <jebaird@gmail.com>
2015-01-30 10:41:29 -0600

    init file commits

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # html5-input-date-value-pollyfill
-provides a simple date string parser that allows the default value of input type date
+
+
+Allows you to set the value of `<input type="date" value="hello world" value="12/1/2015">`
+
+by default the browser will render.
+
+![](http://i.imgur.com/oRmClG4.png)
+
+It will render the desired value
+
+![](http://i.imgur.com/K0EQDp2.png)
+
+
+## Demo
+
+<iframe src="https://rawgit.com/jebaird/html5-input-date-value-pollyfill/master/demo.html">
---
+++ html5-input-date-value-pollyfill.js
@@ -0,0 +1,67 @@
+/*
+    html5 input date value pollyfill
+
+    @author Jesse Baird<jebaird@gmail.com>
+
+    allows you in markup use common date formats as the default value of date inputs
+
+        <input type="date" value="12/25/1900">
+
+    try to guess the format of the date turn it into the format that the date input understands
+    
+    valid format = 2015-05-11
+
+    */
+    function html5InputDateValueParse( dateString ){
+
+        var year, month, day,
+            dateString = dateString
+                        .replace( /[\/\.]/ig , '-' )
+                        .split( '-' )
+                        // pad with leading 0 for date format
+                        .map( function( s ){
+
+                            if( s.length == 1 ){
+                                s = '0' + s;
+                            }
+                            
+                            return s;
+                        });
+
+
+        // try to guess the year
+        if ( dateString[ 0 ].length > 3 ){
+            
+            year = dateString.shift();
+
+            // might be in this format yyyy-mm-dd
+
+
+            if ( parseInt( dateString[ 0 ] ) > 12 ){
+                // what we think is month is really the day
+                day = dateString.pop();
+                month = dateString.pop();
+            }else {
+                month = dateString.shift();
+                day = dateString.pop();
+            }
+
+
+        }else {
+
+            //  mm-dd-yyyy
+            year =  dateString.pop();
+
+            if ( parseInt( dateString[ 1 ] ) > 12 ){
+                // what we think is month is really the day
+                day = dateString.pop();
+                month = dateString.pop();
+            }else {
+                month = dateString.shift();
+                day = dateString.pop();
+            }
+        }
+
+        return [ year, month, day ].join( '-' );
+
+    }
